003d2a68d0114385379eea131d5bf4ec748c0c22
idlesauce <148508202+idlesauce@users.noreply.github.com>
2024-11-21 13:26:12 +0100

    Kill old instance

--- source/main.c
+++ source/main.c
@@ -8,8 +8,58 @@
 #include "server.h"
 #include "libsce_defs.h"
 #include "utils.h"
+#include <signal.h>
+#include <sys/syscall.h>
+
+static pid_t
+find_pid(const char* name) {
+  int mib[4] = {1, 14, 8, 0};
+  pid_t mypid = getpid();
+  pid_t pid = -1;
+  size_t buf_size;
+  uint8_t *buf;
+
+  if(sysctl(mib, 4, 0, &buf_size, 0, 0)) {
+    return -1;
+  }
+
+  if(!(buf=malloc(buf_size))) {
+    return -1;
+  }
+
+  if(sysctl(mib, 4, buf, &buf_size, 0, 0)) {
+    free(buf);
+    return -1;
+  }
+
+  for(uint8_t *ptr=buf; ptr<(buf+buf_size);) {
+    int ki_structsize = *(int*)ptr;
+    pid_t ki_pid = *(pid_t*)&ptr[72];
+    char *ki_tdname = (char*)&ptr[447];
+
+    ptr += ki_structsize;
+    if(!strcmp(name, ki_tdname) && ki_pid != mypid) {
+      pid = ki_pid;
+    }
+  }
+
+  free(buf);
+
+  return pid;
+}
 
 int main() {
+    syscall(SYS_thr_set_name, -1, "ps5debug_dizz.elf");
+
+    int pid;
+    while((pid=find_pid("ps5debug_dizz.elf")) > 0) {
+        if(kill(pid, SIGKILL)) {
+            return -1;
+        }
+        printf_notification("Killed old instance");
+        sleep(1);
+    }
+
     printf_notification("PS5Debug v0.0.1 Loaded\nBy Dizz");
     ScePthread broadcastThread;
     scePthreadCreate(&broadcastThread, 0, (void*)broadcast_thread, 0, "broadcast_thread");
